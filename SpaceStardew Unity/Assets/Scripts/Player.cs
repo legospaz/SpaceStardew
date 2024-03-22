76b6f031ec43d83d6f755324bdc3cdb8e7ee0b9c
BlitzLego <alec.creely@gmail.com>
2024-03-22 17:03:50 -0400

    refactored player movement

---
+++ SpaceStardew Unity/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float moveSpeed = 5f;
+    
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable() 
+    {
+        playerControls.Enable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+}
